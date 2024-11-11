ef194535035356ba945970d1fb7590a1b4123f10
saransh <sethia.saransh.ss@gmail.com>
2024-11-11 18:18:57 +0530

    second commit

---
+++ src/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Home = () => {
+  const API_URL = "https://xcountries-backend.azurewebsites.net/all";
+  const [countries, setCountries] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get(API_URL);
+    const result = await response.data;
+    setCountries(result);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="grid-container">
+      {countries.map((country) => (
+        <div className="grid-item">
+          <img src={country.flag} alt={country.name} />
+          <h3>{country.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
